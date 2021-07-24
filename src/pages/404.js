326a62b402ef1ca8827a2ba87807ceb739c790a4
Dan Bamikiya <dudeawesome732@gmail.com>
2021-07-24 11:30:34 +0000

    refactor: remove unused code :fire:
    * use reusable link
    * use better variable names

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,33 +3,31 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import LINKS from "../utils/constants/links"
 import "../scss/404.scss"
 
-const notFoundStyleHeight = {
-  height: "100vh",
-}
+const NotFoundPage = () => {
+  const styles = {
+    main: {
+      height: "100vh",
+    },
+  }
 
-const notFoundStyleBackground = {
-  backgroundColor: "#1d3557",
+  return (
+    <Layout mainStyle={styles.main}>
+      <SEO title="404: Not found" />
+      <h1 className="not-found-h1">
+        <span className="blast">404</span>: <span className="blast">Not</span>{" "}
+        <span className="blast">Found</span>
+      </h1>
+      <p style={{ textAlign: "center" }}>
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </p>
+      <Link to={LINKS.HOME} className="not-found-home-link">
+        Go back
+      </Link>
+    </Layout>
+  )
 }
-
-const NotFoundPage = () => (
-  <Layout
-    mainStyle={notFoundStyleHeight}
-    notFoundStyle={notFoundStyleBackground}
-  >
-    <SEO title="404: Not found" />
-    <h1 className="not-found-h1">
-      <span className="blast">404</span>: <span className="blast">Not</span>{" "}
-      <span className="blast">Found</span>
-    </h1>
-    <p style={{ textAlign: "center" }}>
-      You just hit a route that doesn&#39;t exist... the sadness.
-    </p>
-    <Link to="/" className="not-found-home-link">
-      Go back
-    </Link>
-  </Layout>
-)
 
 export default NotFoundPage
