4fcc5f80f4b72ca9659cf03031cf9fc78a5992c7
Dan Bamikiya <dudeawesome732@gmail.com>
2021-07-25 19:01:24 +0000

    fix: drop Draft ECMAScript® 2O21 exports :hammer:
    The Draft ECMAScript® 2O21 method of aggregating modules exports doesn't work in gatsby production builds

--- src/utils/constants/svg-paths.js
+++ src/utils/constants/svg-paths.js
@@ -1,23 +1,49 @@
-export * as HTML5 from "../../../svgs/html-5.svg"
-export * as CSS3 from "../../../svgs/css-5.svg"
-export * as Javascript from "../../../svgs/javascript.svg"
-export * as ReactJS from "../../../svgs/react-2.svg"
-export * as Nodejs from "../../../svgs/nodejs.svg"
-export * as Redis from "../../../svgs/redis.svg"
-export * as Sass from "../../../svgs/sass-1.svg"
-export * as Angular from "../../../svgs/angular-icon-1.svg"
-export * as Babel from "../../../svgs/babel-10.svg"
-export * as Bootstrap from "../../../svgs/bootstrap-4.svg"
-export * as Docker from "../../../svgs/docker.svg"
-export * as Express from "../../../svgs/express-109.svg"
-export * as Gatsby from "../../../svgs/gatsby.svg"
-export * as Git from "../../../svgs/git-icon.svg"
-export * as Gulp from "../../../svgs/gulp.svg"
-export * as Heroku from "../../../svgs/heroku.svg"
-export * as MongoDB from "../../../svgs/mongodb-icon-1.svg"
-export * as TailwindCSS from "../../../svgs/tailwindcss.svg"
-export * as Typescript from "../../../svgs/typescript.svg"
-export * as Webpack from "../../../svgs/webpack.svg"
-export * as Agile from "../../../svgs/agile.svg"
-export * as Terraform from "../../../svgs/terraformio-icon.svg"
-export * as HCL from "../../../svgs/hashicorp-configuration-language.svg"
+import HTML5 from "../../../svgs/html-5.svg"
+import CSS3 from "../../../svgs/css-5.svg"
+import Javascript from "../../../svgs/javascript.svg"
+import ReactJS from "../../../svgs/react-2.svg"
+import Nodejs from "../../../svgs/nodejs.svg"
+import Redis from "../../../svgs/redis.svg"
+import Sass from "../../../svgs/sass-1.svg"
+import Angular from "../../../svgs/angular-icon-1.svg"
+import Babel from "../../../svgs/babel-10.svg"
+import Bootstrap from "../../../svgs/bootstrap-4.svg"
+import Docker from "../../../svgs/docker.svg"
+import Express from "../../../svgs/express-109.svg"
+import Gatsby from "../../../svgs/gatsby.svg"
+import Git from "../../../svgs/git-icon.svg"
+import Gulp from "../../../svgs/gulp.svg"
+import Heroku from "../../../svgs/heroku.svg"
+import MongoDB from "../../../svgs/mongodb-icon-1.svg"
+import TailwindCSS from "../../../svgs/tailwindcss.svg"
+import Typescript from "../../../svgs/typescript.svg"
+import Webpack from "../../../svgs/webpack.svg"
+import Agile from "../../../svgs/agile.svg"
+import Terraform from "../../../svgs/terraformio-icon.svg"
+import HCL from "../../../svgs/hashicorp-configuration-language.svg"
+
+export {
+  HTML5,
+  CSS3,
+  Javascript,
+  ReactJS,
+  Nodejs,
+  Redis,
+  Sass,
+  Angular,
+  Babel,
+  Bootstrap,
+  Docker,
+  Express,
+  Gatsby,
+  Git,
+  Gulp,
+  Heroku,
+  MongoDB,
+  TailwindCSS,
+  Typescript,
+  Webpack,
+  Agile,
+  Terraform,
+  HCL,
+}
